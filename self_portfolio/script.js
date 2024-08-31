3d1cf61a87f15ffb9061d5c594f322fbad125fa7
Alisha Khan <162573791+AlishhaKhan@users.noreply.github.com>
2024-08-31 20:46:50 +0500

    Add files via upload

---
+++ self_portfolio/script.js
@@ -0,0 +1,9 @@
+// Simple scroll-to-section function
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+             behavior: 'smooth'
+        });
+    });
+});
